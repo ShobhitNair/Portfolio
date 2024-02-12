5eb1c9a9587e7faefe382ecf792378a4e0d1d2a4
ShobhitNair <shobhit842160@gmail.com>
2024-02-12 13:27:04 +0530

    first commit

---
+++ src/components/DownloadPdfButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const DownloadPDFButton = () => {
+  const handleDownload = () => {
+    const url = 'react-frontend-dev-portfolio\public\Shobhit MernCv.pdf'; 
+    fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'Resume.pdf'); 
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        console.error('Error downloading PDF:', error);
+      });
+  };
+
+  return (
+    <button onClick={handleDownload}>
+      Resume
+    </button>
+  );
+};
+
+export default DownloadPDFButton;
